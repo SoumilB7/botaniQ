6106203dd2b0efe982a48e45317c095152ee54e2
Kiddu77 <kiddu7704@gmail.com>
2025-02-03 21:22:43 +0530

    the classifier model is available locally

--- Python-Backend/main.py
+++ Python-Backend/main.py
@@ -1,8 +1,42 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+import tensorflow as tf
+import numpy as np
+import cv2
+import pickle
 import requests
 import json
 import base64
+
+
+
+IMG_SIZE = 128
+
+# Model load ---------
+model = tf.keras.models.load_model("base_model.keras")
+with open("image_classifier.pkl", "rb") as f:
+    label_encoder = pickle.load(f)
+# --------------------
+
+
+
+def preprocess_base64_image(base64_str):
+    image_bytes = base64.b64decode(base64_str)
+    image_array = np.frombuffer(image_bytes, dtype=np.uint8)
+    img = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+    img = img / 255.0  
+    return img
+def predict_base64_image(base64_str):
+    img = preprocess_base64_image(base64_str)
+    img = np.expand_dims(img, axis=0)  
+    pred = model.predict(img)
+    pred_label = np.argmax(pred, axis=1)[0]
+    class_name = label_encoder.inverse_transform([pred_label])[0]
+    return class_name
+
+
+
 
 app = FastAPI()
 
@@ -17,11 +51,12 @@ async def image_classify(
         request: Request
     ):
     raw_body = await request.body()
-    decoded_body = raw_body.decode("utf-8",errors = "replace")
-    image_cla = json.loads(decoded_body).get("image")
-    image_cla = base64.b64decode(image_cla)
-    content = requests.get()
-    return content
+    decoded_body = raw_body.decode("utf-8", errors="replace")
+    image_base64 = json.loads(decoded_body).get("image")
+    prediction = predict_base64_image(image_base64)
+    print(f"Predicted class: {prediction}")
+
+    return prediction
 
 @app.post("/disease-detect/")
 async def disease_detect(
