32beb85d3006dcf307af7ab8753d33577903d404
Kiddu77 <kiddu7704@gmail.com>
2025-02-04 01:10:44 +0530

    new feature

--- Python-Backend/main.py
+++ Python-Backend/main.py
@@ -13,9 +13,18 @@ import base64
 IMG_SIZE = 128
 
 # Model load ---------
-model = tf.keras.models.load_model("base_model.keras")
+model_img = tf.keras.models.load_model("base_model.keras")
 with open("image_classifier.pkl", "rb") as f:
-    label_encoder = pickle.load(f)
+    label_encoder_img = pickle.load(f)
+
+# -------
+
+model_hlt = tf.keras.models.load_model("plant_health_model.keras")
+with open("planthealth.pkl", "rb") as f:
+    label_encoder_hlt = pickle.load(f)
+with open("scaler.pkl", "rb") as f:
+    scaler_hlt = pickle.load(f)
+
 # --------------------
 
 
@@ -30,12 +39,17 @@ def preprocess_base64_image(base64_str):
 def predict_base64_image(base64_str):
     img = preprocess_base64_image(base64_str)
     img = np.expand_dims(img, axis=0)  
-    pred = model.predict(img)
+    pred = model_img.predict(img)
     pred_label = np.argmax(pred, axis=1)[0]
-    class_name = label_encoder.inverse_transform([pred_label])[0]
+    class_name = label_encoder_img.inverse_transform([pred_label])[0]
     return class_name
 
-
+def predict_health(soil_moisture, ambient_temp, soil_temp, humidity, light_intensity):
+    input_data = np.array([[soil_moisture, ambient_temp, soil_temp, humidity, light_intensity]])
+    input_data = scaler_hlt.transform(input_data) 
+    prediction = model_hlt.predict(input_data)
+    predicted_class = np.argmax(prediction, axis=1)[0]
+    return label_encoder_hlt.inverse_transform([predicted_class])[0]
 
 
 app = FastAPI()
@@ -55,15 +69,24 @@ async def image_classify(
     image_base64 = json.loads(decoded_body).get("image")
     prediction = predict_base64_image(image_base64)
     output = {"Predicted-class": prediction}
+    print(output)
     return output
 
-@app.post("/disease-detect/")
-async def disease_detect(
+@app.post("/health-detect/")
+async def health_detect(
         request: Request
     ):
     raw_body = await request.body()
-    decoded_body = raw_body.decode("utf-8",errors = "replace")
-    image_dis = json.loads(decoded_body).get("image")
-    image_dis = base64.b64decode(image_dis)
-    content = requests.get()
-    return content
+    decoded_body = raw_body.decode("utf-8", errors="replace")
+    data = json.loads(decoded_body)
+
+    soil_moisture = data.get("Soil_Moisture")
+    ambient_temp = data.get("Ambient_Temperature")
+    soil_temp = data.get("Soil_Temperature")
+    humidity = data.get("Humidity")
+    light_intensity = data.get("Light_Intensity")
+
+    if None in [soil_moisture, ambient_temp, soil_temp, humidity, light_intensity]:
+        return {"error": "Missing input data"}
+    predicted_status = predict_health(soil_moisture, ambient_temp, soil_temp, humidity, light_intensity)
+    return {"Predicted_Health_Status": predicted_status}
